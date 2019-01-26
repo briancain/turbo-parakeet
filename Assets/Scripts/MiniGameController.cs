0e9d94a8f78c763ff39492daf2368a7cd9e43771
Preben Ingvaldsen <pastelmobilesuit@github.com>
2019-01-26 13:06:45 -0800

    Add a minigame timer
    Add a 10-second minigame timer that counts down as the minigame
    proceeds.

--- Assets/Scripts/MiniGameController.cs
+++ Assets/Scripts/MiniGameController.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MiniGameController : MonoBehaviour
 {
+  public Text timerText;
+
   protected bool gameStarted = false;
   private float timer;
   private float gameTime = 10.0f;
@@ -21,6 +24,7 @@ public class MiniGameController : MonoBehaviour
     if (gameStarted)
     {
       timer -= Time.deltaTime;
+      timerText.text = ":" + Mathf.Ceil(timer);
       Debug.Log("Minigame time: " + timer);
 
 
@@ -38,11 +42,13 @@ public class MiniGameController : MonoBehaviour
     {
       gameStarted = true;
       timer = gameTime;
+      timerText.enabled = true;
     }
   }
 
   public void EndGame()
   {
     gameStarted = false;
+    timerText.enabled = false;
   }
 }
