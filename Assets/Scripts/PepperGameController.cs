8ca41c0cc513c7f75a99a3981b1701539aafeaaf
Preben Ingvaldsen <pastelmobilesuit@github.com>
2019-01-26 15:17:31 -0800

    Add a sliding Pepper Bar during Pepper game
    During the Pepper minigame, add a sliding bar with a target to hit

---
+++ Assets/Scripts/PepperGameController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PepperGameController : MiniGameController
+{
+
+  public Slider stopper;
+  public Slider marker;
+  public GameObject pepperBar;
+
+  // Start is called before the first frame update
+  protected override void Start()
+  {
+    base.Start();
+  }
+
+  // Update is called once per frame
+  protected override void Update()
+  {
+    base.Update();
+
+    if (gameStarted)
+    {
+      stopper.value = 0.5f + 0.5f * Mathf.Sin(1.0f * Time.time);
+    }
+  }
+
+  public override void StartGame()
+  {
+    if (!gameStarted)
+    {
+      pepperBar.SetActive(true);
+      marker.value = Random.Range(0.1f, 0.9f);
+    }
+
+    base.StartGame();
+  }
+
+  protected override void EndGame()
+  {
+    base.EndGame();
+    pepperBar.SetActive(false);
+  }
+}
