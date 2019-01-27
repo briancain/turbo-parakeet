281f457466c433a7625f04fdb90840d2bd6cd35d
Brian Cain <bcain@hashicorp.com>
2019-01-26 16:12:45 -0800

    Add some carving drawing

--- Assets/Scripts/MeatMiniGameScript.cs
+++ Assets/Scripts/MeatMiniGameScript.cs
@@ -6,30 +6,24 @@ public class MeatMiniGameScript : MonoBehaviour
 {
   private float mouseX;
   private float mouseY;
+  private Vector2 mousePosition;
 
-  private BoxCollider2D bc;
-
-  // Use this for initialization
-  void Start () {
-    bc = gameObject.GetComponent<BoxCollider2D>();
-  }
-
-  // Update is called once per frame
-  void Update () {
-    mouseX = Input.mousePosition.x;
-    mouseY = Input.mousePosition.y;
-    //Debug.Log(mouseX + "; " + mouseY);
-  }
+  [SerializeField] private GameObject line;
 
   // Check if mini game was clicked
   // Only called if initiated on collider
   void OnMouseDrag() {
     Debug.Log(mouseX + "; " + mouseY);
     Debug.Log("Slicing meat....");
+    mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    Instantiate(line, mousePosition, Quaternion.Euler(0.0f, 0.0f, 0.0f));
   }
 
-  void OnMouseUp() {
-    // check if path is complete, if so you win, otherwise fail
-    Debug.Log("Let go!");
+  //void OnMouseUp() {
+  //  // check if path is complete, if so you win, otherwise fail
+  //  Debug.Log("Let go!");
+  //}
+
+  void Update () {
   }
 }
