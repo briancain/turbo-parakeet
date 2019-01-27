076e008560fc3073ad4de1b72b0a46e072860773
Brian Cain <bcain@hashicorp.com>
2019-01-26 19:45:30 -0800

    Add the meat

--- Assets/Scripts/MeatMiniGameScript.cs
+++ Assets/Scripts/MeatMiniGameScript.cs
@@ -7,35 +7,65 @@ public class MeatMiniGameScript : MonoBehaviour
   private float mouseX;
   private float mouseY;
   private Vector2 mousePosition;
+  private float goalDistance;
 
   private int totalSlices;
 
   [SerializeField] private GameObject line;
 
+  [SerializeField] public List<GameObject> slicesList;
+
   private List<GameObject> lineList;
 
   void Update () {
+    mouseX = Input.mousePosition.x;
+    mouseY = Input.mousePosition.y;
+
+    if (Input.GetMouseButton(0)){ //Or use GetKey with key defined with mouse button
+
+      mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      GameObject localLineObj = Instantiate(line, mousePosition, Quaternion.Euler(0.0f, 0.0f, 0.0f));
+      Destroy(localLineObj.gameObject, 1);
+
+      //Debug.Log(mouseX + "; " + mouseY);
+      //Debug.Log("Slicing meat....");
+      CheckSlices();
+    }
+  }
+
+  void CheckSlices() {
+    Vector3 mousePos = new Vector3(mouseX, mouseY, 0f);
+    Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    pz.z = 0;
+    foreach(GameObject s in slicesList) {
+      float distance = Vector3.Distance(s.transform.position, pz);
+      if(distance <= goalDistance) {
+        totalSlices--;
+        if(totalSlices <= 0) {
+          GameOver();
+        }
+      }
+    }
+
+  }
+
+  void OnCollisionEnter2D(Collision2D col) {
+    Debug.Log("oops");
   }
 
   void Start() {
-    lineList = new List<GameObject>();
-    totalSlices = 0;
+    totalSlices = 75;
+    goalDistance = 1f;
   }
 
   // connect the dots....
   void OnMouseDrag() {
-    Debug.Log(mouseX + "; " + mouseY);
-    Debug.Log("Slicing meat....");
-
-    mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    GameObject localLineObj = Instantiate(line, mousePosition, Quaternion.Euler(0.0f, 0.0f, 0.0f));
-    lineList.Add(localLineObj);
 
     // NEED TO CHECK COLLISIONSSSSS OF POINTS
   }
 
-  public void OnDestroy() {
-    lineList.Clear();
+  private void GameOver() {
+    Debug.Log("game Over!!");
   }
 
   void OnMouseUp() {
