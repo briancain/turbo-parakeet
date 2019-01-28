7e5bf1ddede9e71a00699f16790290f65d057422
Brian Cain <bcain@hashicorp.com>
2019-01-27 18:01:19 -0800

    Add title screen

---
+++ Assets/TitlePageController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitlePageController : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetMouseButton(0)){ //Or use GetKey with key defined with mouse button
+      SceneManager.LoadScene("Dev");
+    }
+  }
+}
